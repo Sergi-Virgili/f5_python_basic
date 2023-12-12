3e56fd32cd48705021d7edae28aa1e1335c40d05
archima20 <shima.naderii@gmail.com>
2023-12-12 12:03:34 +0100

    the secode exercise

--- src/_08_oop.py
+++ src/_08_oop.py
@@ -21,17 +21,26 @@ my_book.mostrar_informacion()
 # Crea una clase `Vehiculo` con atributos `marca` y `modelo`.
 # Luego crea una clase `Coche` que herede de `Vehiculo` y añade un atributo `cilindrada`.
 
+class Vehiculo:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+        pass
 
-# --------------------------------------------------
-# YOU CODE HERE
-# ---------------------------------------------------
-
+class Coche(Vehiculo):
+    def __init__(self, brand, model, cylander_capacity):
+        super().__init__(brand, model)
+        self.cylander_capacity = cylander_capacity
+        
 
 # Ejercicio 3: Encapsulación
 # Crea una clase CuentaBancaria que tenga dos atributos privados:
 # _saldo y _titular. Implementa métodos para depositar y retirar dinero, 
 # además de un método para ver el saldo actual.
 
+# Create a class BankAccount that has two private attributes:
+# _balance and _holder. Implement methods to deposit and withdraw money,
+# as well as a method to view the current balance.
 
 # --------------------------------------------------
 # YOU CODE HERE
